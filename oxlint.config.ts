e0aaedb3bcd1fb4486c8fbb56343be602b2ebf5b
Errilaz <errilaz@errilaz.org>
2026-09-25 15:20:20 +0200

    chore: update deps

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -68,6 +68,7 @@ export default defineConfig({
     "typescript/prefer-function-type": "off",
     "unicorn/no-process-exit": "off",
     "unicorn/prefer-string-raw": "off",
+    "unicorn/import-style": "warn",
     "unicorn/relative-url-style": "off",
     "unicorn/require-post-message-target-origin": "off",
     "unicorn/text-encoding-identifier-case": "off",
